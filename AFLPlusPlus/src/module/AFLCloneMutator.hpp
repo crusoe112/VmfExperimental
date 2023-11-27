fa4b753f1a7a8af04da28bd0bb8c0199c5aac5eb
mbohler <mbohler@vigilantsys.com>
2023-11-27 14:48:58 -0500

    LibAFL-legacy test

--- AFLPlusPlus/src/module/AFLCloneMutator.hpp
+++ AFLPlusPlus/src/module/AFLCloneMutator.hpp
@@ -38,7 +38,8 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wwrite-strings"
 extern "C" {
-    #include "LibAFL-legacy/rand.h"
+    //#include "LibAFL-legacy/rand.h"
+    #include /root/VaderModularFuzzer/vmf/external/bookworm/include/LibAFL-legacy
 }
 #pragma GCC diagnostic pop
 
