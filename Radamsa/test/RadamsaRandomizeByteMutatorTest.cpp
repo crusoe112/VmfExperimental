fd7975fc984a4cc44de4836c7f6dfdce50d4c110
GabeClark99 <54868591+GabeClark99@users.noreply.github.com>
2024-12-09 21:26:30 -0500

    RandomizeByte TestRandomize

---
+++ Radamsa/test/RadamsaRandomizeByteMutatorTest.cpp
@@ -0,0 +1,123 @@
+/* Notes for future me:
+* solved missing Radamsa definitions by adding "target_link_directories" in unittest cmakelists, adding .../vmf_install/plugins, then adding entry for radamsa lib to "target_link_libraries"
+*/
+
+#include "gtest/gtest.h"
+#include "../../../VaderModularFuzzer/test/unittest/ModuleTestHelper.hpp"
+#include "SimpleStorage.hpp"
+#include "../vmf/src/modules/common/mutator/RadamsaRandomizeByteMutator.hpp"
+
+// using namespace vmf;
+using vmf::StorageModule;
+using vmf::StorageRegistry;
+using vmf::ModuleTestHelper;
+using vmf::TestConfigInterface;
+using vmf::SimpleStorage;
+using vmf::StorageEntry;
+using vmf::RadamsaRandomizeByteMutator;
+using vmf::BaseException;
+
+// #define GTEST_COUT std::cerr << "[          ] [ INFO ]"
+
+class RadamsaRandomizeByteMutatorTest : public ::testing::Test {
+  protected:
+    RadamsaRandomizeByteMutatorTest() 
+    {
+      storage = new SimpleStorage("storage");
+      registry = new StorageRegistry("TEST_INT", StorageRegistry::INT, StorageRegistry::ASCENDING);
+      metadata = new StorageRegistry();
+      testHelper = new ModuleTestHelper();
+      theMutator = new RadamsaRandomizeByteMutator("RadamsaRandomizeByteMutator");
+      config = testHelper -> getConfig();
+    }
+
+    ~RadamsaRandomizeByteMutatorTest() override {}
+
+    void SetUp() override {
+      testCaseKey = registry->registerKey(
+          "TEST_CASE", 
+          StorageRegistry::BUFFER, 
+          StorageRegistry::READ_WRITE
+      );
+      // int_key = registry->registerKey(
+      //     "TEST_INT",
+      //     StorageRegistry::INT,
+      //     StorageRegistry::READ_WRITE
+      // );
+      // normalTag = registry->registerTag(
+      //     "RAN_SUCCESSFULLY",
+      //     StorageRegistry::WRITE_ONLY
+      // );
+      // // registry->validateRegistration();
+      storage->configure(registry, metadata);
+      theMutator->init(*config);
+      theMutator->registerStorageNeeds(*registry);
+      theMutator->registerMetadataNeeds(*metadata);
+  }
+
+    void TearDown() override {
+      delete registry;
+      delete metadata;
+      delete storage;
+    }
+
+    RadamsaRandomizeByteMutator* theMutator;
+    StorageModule* storage;
+    StorageRegistry* registry;
+    StorageRegistry* metadata;
+    ModuleTestHelper* testHelper;
+    TestConfigInterface* config;
+    int testCaseKey;
+};
+
+TEST_F(RadamsaRandomizeByteMutatorTest, TestBufferSize)
+{
+  // Test that the mutated entry is one byte larger than the base entry
+
+  StorageEntry* baseEntry = storage->createNewEntry();
+  StorageEntry* modEntry = storage->createNewEntry();
+
+  char* buff = baseEntry->allocateBuffer(testCaseKey, 12);
+  buff[0] = 'T';
+  buff[1] = 'E';
+  buff[2] = 'S';
+  buff[3] = 'T';
+  buff[4] = '1';
+  buff[5] = '2';
+  buff[6] = '3';
+  buff[7] = '4';
+  buff[8] = '5';
+  buff[9] = '6';
+  buff[10] = '7';
+  buff[11] = '8';
+
+  try{
+      theMutator->mutateTestCase(*storage, baseEntry, modEntry, testCaseKey);
+  } 
+  catch (BaseException e)
+  {
+    FAIL() << "Exception thrown: " << e.getReason();
+  }
+
+  EXPECT_EQ(baseEntry->getBufferSize(testCaseKey) + 1, modEntry->getBufferSize(testCaseKey));
+}
+
+TEST_F(RadamsaRandomizeByteMutatorTest, TestRandomize)
+{
+  StorageEntry* baseEntry = storage->createNewEntry();
+  StorageEntry* modEntry = storage->createNewEntry();
+
+  char* buff = baseEntry->allocateBuffer(testCaseKey, 1);
+  buff[0] = '4';
+
+  try{
+      theMutator->mutateTestCase(*storage, baseEntry, modEntry, testCaseKey);
+  } 
+  catch (BaseException e)
+  {
+    FAIL() << "Exception thrown: " << e.getReason();
+  }
+
+  char* modBuff = modEntry->getBufferPointer(testCaseKey);
+  EXPECT_NE(modBuff[0], '4');
+}
