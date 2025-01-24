86f1f26a098c4cb50689858f300aa6035c31e1c5
GabeClark99 <54868591+GabeClark99@users.noreply.github.com>
2025-01-24 15:15:46 -0500

    LineMutatorBase
    stub class for LineMutatorBase and telling mutators to inherit

--- Radamsa/vmf/src/modules/common/mutator/RadamsaRepeatLineMutator.hpp
+++ Radamsa/vmf/src/modules/common/mutator/RadamsaRepeatLineMutator.hpp
@@ -31,7 +31,7 @@
 #include "MutatorModule.hpp"
 #include "StorageEntry.hpp"
 #include "RuntimeException.hpp"
-#include "mutationBase.hpp"
+#include "RadamsaLineMutatorBase.hpp"
 #include "VmfRand.hpp"
 
 namespace vmf
@@ -39,7 +39,7 @@ namespace vmf
 /**
  *
  */
-class RadamsaRepeatLineMutator: public MutatorModule, public MutationBase
+class RadamsaRepeatLineMutator: public MutatorModule, public RadamsaLineMutatorBase
 {
     public:
 
