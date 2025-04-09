51d81e66b04c36555ae110a2d75af94f5d0f2f87
GabeClark99 <54868591+GabeClark99@users.noreply.github.com>
2025-04-09 09:29:35 -0400

    Mutator stubs
    mutator names are subject to change once I figure out what each one is actually doing

---
+++ Radamsa/vmf/src/modules/common/mutator/RadamsaJumpSimilarMutator.cpp
@@ -0,0 +1,95 @@
+/* =============================================================================
+ * Vader Modular Fuzzer (VMF)
+ * Copyright (c) 2021-2024 The Charles Stark Draper Laboratory, Inc.
+ * <vmf@draper.com>
+ *  
+ * Effort sponsored by the U.S. Government under Other Transaction number
+ * W9124P-19-9-0001 between AMTC and the Government. The U.S. Government
+ * Is authorized to reproduce and distribute reprints for Governmental purposes
+ * notwithstanding any copyright notation thereon.
+ *  
+ * The views and conclusions contained herein are those of the authors and
+ * should not be interpreted as necessarily representing the official policies
+ * or endorsements, either expressed or implied, of the U.S. Government.
+ *  
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 (only) as 
+ * published by the Free Software Foundation.
+ *  
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *  
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *  
+ * @license GPL-2.0-only <https://spdx.org/licenses/GPL-2.0-only.html>
+ * ===========================================================================*/
+ /**
+  *
+  */
+#include "RadamsaJumpSimilarMutator.hpp"
+#include "RuntimeException.hpp"
+#include <random>
+#include <algorithm>
+
+using namespace vmf;
+
+#include "ModuleFactory.hpp"
+REGISTER_MODULE(RadamsaJumpSimilarMutator);
+
+/**
+ * @brief Builder method to support the ModuleFactory
+ * Constructs an instance of this class
+ * @return Module* - Pointer to the newly created instance
+ */
+Module* RadamsaJumpSimilarMutator::build(std::string name)
+{
+    return new RadamsaJumpSimilarMutator(name);
+}
+
+/**
+ * @brief Initialization method
+ *
+ * @param config - Configuration object
+ */
+void RadamsaJumpSimilarMutator::init(ConfigInterface& config)
+{
+
+}
+
+/**
+ * @brief Construct a new RadamsaJumpSimilarMutator::RadamsaJumpSimilarMutator object
+ *
+ * @param name The of the name module
+ */
+RadamsaJumpSimilarMutator::RadamsaJumpSimilarMutator(std::string name) : MutatorModule(name)
+{
+    // rand->randInit();
+}
+
+/**
+ * @brief Destroy the RadamsaJumpSimilarMutator::RadamsaJumpSimilarMutator object
+ *
+ */
+RadamsaJumpSimilarMutator::~RadamsaJumpSimilarMutator()
+{
+
+}
+
+/**
+ * @brief Register the storage needs for this module
+ *
+ * @param registry - StorageRegistry object
+ */
+void RadamsaJumpSimilarMutator::registerStorageNeeds(StorageRegistry& registry)
+{
+    // This module does not register for a test case buffer key, because mutators are told which buffer to write in storage
+    // by the input generator that calls them
+}
+
+void RadamsaJumpSimilarMutator::mutateTestCase(StorageModule& storage, StorageEntry* baseEntry, StorageEntry* newEntry, int testCaseKey)
+{
+    // TODO: Add the mutator contents here
+}
